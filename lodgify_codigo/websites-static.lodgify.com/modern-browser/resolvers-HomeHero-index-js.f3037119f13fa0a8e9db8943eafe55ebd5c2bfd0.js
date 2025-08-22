66ca29a600407282d3177c30ea767ea0b18eeb0a
Trakaleh <Trakaleh@gmail.com>
2025-08-22 18:52:46 +0200

    Delete lodgify_codigo directory

--- lodgify_codigo/websites-static.lodgify.com/modern-browser/resolvers-HomeHero-index-js.f3037119f13fa0a8e9db8943eafe55ebd5c2bfd0.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.__LOADABLE_LOADED_CHUNKS__=self.__LOADABLE_LOADED_CHUNKS__||[]).push([[2769],{3350:(_,A,D)=>{D.r(A),D.d(A,{default:()=>L.Component});var L=D(5315)}}]);
--- lodgify_codigo/websites-static.lodgify.com/modern-browser/resolvers-Html-index-js.d4d8b64bdb7564a9f89eef47add58cccf72ce4bf.js
+++
@@ -1,1 +0,0 @@
-(window.__LOADABLE_LOADED_CHUNKS__=window.__LOADABLE_LOADED_CHUNKS__||[]).push([[32],{610:function(t,n,e){"use strict";e.r(n);var i=e(0),r=e.n(i),o=e(1),_=e(357);const c=t=>{let{content:n,componentId:e}=t;return r.a.createElement(_.RichTextWidget,{id:e,html:n})};c.propTypes={content:o.string.isRequired,componentId:o.string.isRequired},e.d(n,"default",(function(){return c}))}}]);
